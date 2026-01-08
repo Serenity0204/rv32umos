7b6b195b5c0c39323fe624026a7264bef3688e92
Serenity0204 <yuhenglin0204@gmail.com>
2026-01-07 18:18:41 -0800

    fixed comment

--- core/common/Common.hpp
+++ core/common/Common.hpp
@@ -19,7 +19,7 @@ const Addr MEMORY_SIZE = 1024 * 1024 * 128;
 // 1kb
 const Addr PAGE_SIZE = 4096;
 
-// 0x88000000
+// 0x08000000
 const Addr STACK_TOP = MEMORY_BASE + MEMORY_SIZE;
 
 // 8MB below top
